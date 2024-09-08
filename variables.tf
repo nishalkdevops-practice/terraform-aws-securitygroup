abd9a17871d7ad29082020c9b7557aae7fdc7a01
nishal <nishalk9399@gmail.com>
2024-09-08 12:04:39 +0530

    SG module developed and roboshop-infra-user is a user module

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "sg_name" {
+  
+}
+
+variable "sg_description" {
+  
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "sg_ingress_rules" {
+    default = []
+  
+}
+
+variable "project_name" {
+  
+}
+
+variable "common_tags" {
+  
+}
+
+variable "sg_tags" {
+  
+}
+
+
+
+
